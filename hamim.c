a956144a56aad4f4239697f59e756a3728e81dd8
gomes1307 <162542254+gomes1307@users.noreply.github.com>
2024-03-07 12:11:54 +0600

    Add files via upload

---
+++ hamim.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+printf("-------------");
+printf("             ");
+printf("-------------\n");
+printf("|           |");
+printf("             ");
+printf("|           |\n");
+printf("|           |");
+printf(">>---------->");
+printf("|           |\n");
+
+printf("-------------");
+printf("             ");
+printf("-------------");
+
+}
