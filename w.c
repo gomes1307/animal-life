327ec751dd85a03144dc5f8339eba8fad3bceba7
gomes1307 <162542254+gomes1307@users.noreply.github.com>
2024-03-08 12:11:12 +0600

    Update w.c

--- w.c
+++ w.c
@@ -1,12 +1,11 @@
-
- #include<stdio.h>
+#include<stdio.h>
 #include<conio.h>
 void main()
 {
  int a,b,c,d,m,n,Dr;
 
  float x1,x2;
-
+ 
  printf("Enter the value of a, b, c, d, m, n: ");
  scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&m,&n);
 
